93266143b336ec43f42daf21843ec25bf418e84f
Matti Ristimaki <ristimaki.matti@gmail.com>
2017-10-08 19:28:46 +0300

    quick update

---
+++ database/knexfile-template.js
@@ -0,0 +1,12 @@
+module.exports = {
+    
+      development: {
+        client: 'pg',
+        connection: {
+            host : '127.0.0.1',
+            user : 'postgres',
+            password : 'password',
+            database : 'mm-futis'
+        }
+      }
+    }
