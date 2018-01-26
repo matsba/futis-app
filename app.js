f43f0d6edaa0a1caa98c3a25626fc0bda922b655
Matti Ristimaki <ristimaki.matti@gmail.com>
2018-01-26 19:43:05 +0200

    Added favicon

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const app = express()
 const path = require('path')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 const flash = require('express-flash')
-//var moment = require('moment')
+const favicon = require('serve-favicon')
 //var cookieParser = require('cookie-parser')
+
+const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -41,3 +42,5 @@ app.use(require('./controllers'))
 app.listen(app.get('port'), () => {
 	console.log('App is running on port ', app.get('port'))
 })
+
+app.use(favicon(__dirname + '/public/img/favicon.ico'))
