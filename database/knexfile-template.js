58dc3402808b7c787e04b8037e6af33bf98475b0
Matti Ristimaki <ristimaki.matti@gmail.com>
2018-01-27 12:04:51 +0200

    Database file changes

--- database/knexfile-template.js
+++ database/knexfile-template.js
@@ -3,10 +3,11 @@ module.exports = {
       development: {
         client: 'pg',
         connection: {
-            host : '127.0.0.1',
-            user : 'postgres',
-            password : 'password',
-            database : 'mm-futis'
+            host : process.env.ENV_DB_HOST,
+            port: 5432,
+            user : process.env.ENV_DB_USER,
+            password : process.env.ENV_DB_USER_PASSWORD,
+            database : process.env.ENV_DATABASE
         }
       }
     }
