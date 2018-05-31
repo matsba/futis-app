cf3bbe71c20c692fb7b5d35245a69b2731008832
Matti Ristimaki <ristimaki.matti@gmail.com>
2018-05-31 21:19:21 +0300

    Added page for tournament games that was missing

--- controllers/tournamentController.js
+++ controllers/tournamentController.js
@@ -7,6 +7,28 @@ const Pools = require('../models/pools')
 const moment = require('moment')
 const util = require('util')
 
+
+router.get('/games/:id', async (req, res) => {
+    if (!User.authenticateUser(req)) {
+        return res.redirect('/')
+    }    
+
+    const userId = req.session.user.id
+    const tournamentId = req.params.id
+
+    try {
+        const tournament = await Tournament.getByIdAsync(tournamentId)
+        const games = Game.getCountryCodeForTeams(await Game.getGames(tournamentId))
+
+        res.render('tournament/games', { 
+            tournament,
+            games
+         })
+    } catch (error) {
+        res.status(500).send('Internal_server_error')
+    }    
+})
+
 router.get('/:id', async (req, res) => {
     if (!User.authenticateUser(req)) {
         return res.redirect('/')
